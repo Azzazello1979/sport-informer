c9c126ccdffe4b60a7eb54595a98781a577ee582
Balint H <balint.haui@gmail.com>
2020-10-28 10:31:56 +0100

    competitions component subscribed for competitions from service

--- front-end/src/app/components/competitions/competitions.component.ts
+++ front-end/src/app/components/competitions/competitions.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/services/main.service';
 
 @Component({
   selector: 'app-competitions',
   templateUrl: './competitions.component.html',
-  styleUrls: ['./competitions.component.css']
+  styleUrls: ['./competitions.component.css'],
 })
 export class CompetitionsComponent implements OnInit {
+  private allCompetitions: any[] = [];
 
-  constructor() { }
+  constructor(private mainsrvc: MainService) {}
 
-  ngOnInit(): void {
+  getAllCompetitions() {
+    this.mainsrvc.getAllCompetitions();
   }
 
+  ngOnInit() {
+    this.getAllCompetitions();
+    this.mainsrvc.allCompetitionsObservable().subscribe((news) => {
+      this.allCompetitions = news;
+      console.log(this.allCompetitions);
+    });
+  }
 }
--- front-end/src/app/services/main.service.ts
+++ front-end/src/app/services/main.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MainService {
-  constructor() {}
+  private requestConfig = {
+    headers: {
+      'X-Auth-Token': '392c41296a2e43bb80bb5233b2470e33',
+    },
+  };
+  private competitionsResponse: any = {};
+  private competitions: Array<any> = [];
+  private competitionsChanged = new BehaviorSubject<any[]>(this.competitions);
+
+  constructor(private http: HttpClient) {}
+
+  allCompetitionsObservable() {
+    return this.competitionsChanged.asObservable();
+  }
+
+  getAllCompetitions() {
+    return this.http
+      .get('https://api.football-data.org/v2/competitions', this.requestConfig)
+      .subscribe(
+        (response) => {
+          this.competitionsResponse = response;
+          this.competitions = this.competitionsResponse.competitions;
+          this.competitionsChanged.next(this.competitions);
+        },
+        (err) => {
+          return console.log(err.message);
+        }
+      );
+  }
 }
