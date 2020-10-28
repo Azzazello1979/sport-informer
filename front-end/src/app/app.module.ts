5154923d14272dbd8d9173fc062c0862d250f728
Balint H <balint.haui@gmail.com>
2020-10-28 10:20:24 +0100

    bringing in modules

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,18 +1,20 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+// services
+import { MainService } from './services/main.service';
+
+// components
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, RouterModule],
+  providers: [MainService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
