446a8d6274cd2385c248c1eb6b7a7770883d26e7
Balint H <balint.haui@gmail.com>
2020-10-29 11:23:37 +0100

    header notification on selected league

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/services/main.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front-end';
+  selectedCompetition: string = '';
+
+  constructor(private mainsrvc: MainService) {}
+
+  ngOnInit() {
+    this.mainsrvc.selectedCompetitionObservable().subscribe((news) => {
+      this.selectedCompetition = news;
+    });
+  }
 }
